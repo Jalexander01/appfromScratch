e5c31e84e1d557457760d265464f69ca06f6c0a8
James Alexander <60295314+Jalexander01@users.noreply.github.com>
2021-06-08 10:08:52 -0500

    updating the app creating hundsband todolist

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    {" "}
+    <h1>JSX H1 Heading</h1>
+    <ul>
+      {" "}
+      Hundsband's todolist
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
